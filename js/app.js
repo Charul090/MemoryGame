1b6b7461883f023356db6df0fb3a21a743381bec
Charul Bari <baricharul9@gmail.com>
2018-08-29 13:52:53 +0530

    feat:Add Cards Shuffle function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 /*
  * Create a list that holds all of your cards
  */
+ //@description To store list of cards in variable
 let cardList = document.getElementsByClassName('card');
-
+let cards=[...cardList];
+//@description To store deck of cards in a variable
+let deck=document.querySelector('.deck')
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+//@description Shuffles card every time page is loaded and refreshed
+document.body.onload=start();
+
+//@description Cards shuffling logic
+function start(){
+  shuffle(cards);
+  for(i=0;i<cards.length;i++){
+    const q =cards[i].classList;
+    q.remove('match','show','open');
+    deck.appendChild(cards[i]);
+  }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
